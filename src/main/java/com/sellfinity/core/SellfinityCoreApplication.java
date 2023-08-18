157964a7ac979809811127341319e4966bc1ff33
Waldo Sobrino <waldo2810@gmail.com>
2023-08-17 22:58:53 -0500

    init project

---
+++ src/main/java/com/sellfinity/core/SellfinityCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sellfinity.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellfinityCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellfinityCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sellfinity/core/SellfinityCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellfinity.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellfinityCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
